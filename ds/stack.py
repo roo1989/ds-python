527eede580db959a93bd8dd1765bce24fea288d5
roo1989 <ragnar.orn@gmail.com>
2021-09-30 16:18:06 +0200

    Implement simple push and pop functionality

--- ds/stack.py
+++ ds/stack.py
@@ -1,6 +1,14 @@
 class Stack:
     def __init__(self):
         self._storage = []
-    
+
     def __len__(self):
         return len(self._storage)
+
+    def push(self, value):
+        self._storage.append(value)
+
+    def pop(self):
+        if len(self._storage) > 0:
+            return self._storage.pop()
+        return None
