cee1449ccde7814caad0598ac7db7374605d1e36
roo1989 <ragnar.orn@gmail.com>
2021-09-30 16:17:37 +0200

    Add tests for pop and push, and add fixtures

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,7 +1,26 @@
 from ds.stack import Stack
+import pytest
+
+
+@pytest.fixture
+def stack():
+    return Stack()
 
 
 def test_constructor():
-     s = Stack()
-     assert isinstance(s, Stack)
-     assert len(s) == 0
+    s = Stack()
+    assert isinstance(s, Stack)
+    assert len(s) == 0
+
+
+def test_push(stack):
+    stack.push(3)
+    assert len(stack) == 1
+
+def test_pop(stack):
+     stack.push("hello")
+     stack.push("world")
+
+     assert stack.pop() == "world"
+     assert stack.pop() == "hello"
+     assert stack.pop() is None
